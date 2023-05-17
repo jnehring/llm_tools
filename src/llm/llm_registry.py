858a183cd4bda95fb1b6cac34d349ddb35edaf4a
Jan Nehring <jan.nehring@dfki.de>
2023-05-17 15:03:24 +0200

    add pythia model

--- src/llm/llm_registry.py
+++ src/llm/llm_registry.py
@@ -4,6 +4,10 @@ def load_llama(size):
     from llm.wrapper.llama.llama_wrapper import LLamaWrapper
     return LLamaWrapper()
 
+def load_pythia():
+    from llm.wrapper.pythia.pythia_wrapper import PythiaWrapper
+    return PythiaWrapper()
+
 def init_remote_http_llm(app):
     if "api_url" not in app.get_args():
         raise Exception("--url parameter not set.")
@@ -13,6 +17,7 @@ llm_registry = {
     "openai_davinci": lambda app : OpenAIDavinci(),
     "dummy_llm": lambda app : DummyLLM(),
     "llama-7b": lambda app : load_llama("7B"),
+    "pythia": lambda app : load_pythia(),
     "http": init_remote_http_llm
 }
 
