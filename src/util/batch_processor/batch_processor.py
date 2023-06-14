45748407d979b3293da9220b61570ee1a4ad470f
Jan Nehring <jan.nehring@dfki.de>
2023-06-14 10:42:26 +0200

    update batch processor

--- src/util/batch_processor/batch_processor.py
+++ src/util/batch_processor/batch_processor.py
@@ -11,44 +11,45 @@ def call_llm(input_str, api_url="http://localhost:5000/api/generate"):
     x = requests.post(api_url, json = body)
     return x.json()
 
-def batch_process(api, file_in, file_out):
+def batch_process(args):
     
-    df = pd.read_csv(file_in, dtype=str)
-    if "input" not in df.columns:
+    df = pd.read_csv(args.input_file, dtype=str)
+    if args.input_column not in df.columns:
         raise ValueError("Input csv must have an 'input' column")
     
     # Create a column if it doesn't exist already
-    df['output'] = df.get('output', None) 
-    with tqdm(df.itertuples(), total = df.shape[0], ascii=True, desc=f"processing {file_in}", unit="rows") as pbar:
+    df[args.output_column] = df.get(args.output_column, None) 
+    with tqdm(df.itertuples(), total = df.shape[0], ascii=True, desc=f"processing {args.input_file}", unit="rows") as pbar:
         for row in pbar:
             #Skip rows which already have an output
-            if not pd.isnull(df.loc[row.Index, 'output']): 
+            if not pd.isnull(df.loc[row.Index, args.output_column]): 
                 pbar.update()
                 continue 
-            df.loc[row.Index, 'output'] = call_llm(row.input, api)['response']
-            df.to_csv(file_out, index=False)
+            input_str = row.__getattribute__(args.input_column)
+            df.loc[row.Index, args.output_column] = call_llm(input_str, args.api)['response']
+            df.to_csv(args.output_file, index=False)
             # time.sleep(1)
             pbar.update()
 
 
     timer = pbar.format_dict["elapsed"]
-    print(f"Processed {df.shape[0]} rows in {timer:.4} seconds. ({df.shape[0]/timer:.4} rows/second)")
-    print(f"Saved results into {file_out}")
+    print(f"Processed {df.shape[0]} documents in {timer:.4} seconds. ({df.shape[0]/timer:.4} documents/second)")
+    print(f"Saved results into {args.output_file}")
 
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser(prog="batch_processor",
                                  description="Process a csv data in api")
     
-    ap.add_argument("--api", type=str, help= "API URL")
-    ap.add_argument("-i", "--input", type=str, help = "input file path")
-    ap.add_argument("-o", "--output", type=str, help = "output file path")
+    ap.add_argument("--api", type=str, default="http://localhost:5000/api/generate", help= "API URL")
+    ap.add_argument("-i", "--input_file", default="input.csv", type=str, help = "input file path")
+    ap.add_argument("-o", "--output_file", default="output.csv", type=str, help = "output file path")
+    ap.add_argument("-ic", "--input_column", default="input", type=str, help = "name of the input data column")
+    ap.add_argument("-oc", "--output_column", default="output", type=str, help = "name of the output data column")
 
     args = ap.parse_args()
-    api = "http://localhost:5000/api/generate" if args.api is None else args.api
-    file_in = "input.csv" if args.input is None else args.input
-    file_out = "output.csv" if args.output is None else args.output
+
     try:
-        batch_process(api, file_in, file_out)
+        batch_process(args)
     except KeyboardInterrupt:
         pass
